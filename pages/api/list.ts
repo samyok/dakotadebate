eb94e356ddabca17bc7baac3989fb0c37d3f60f8
Samyok Nepal <nepalsamyok@gmail.com>
2021-06-26 23:24:35 -0500

    add csv instead of json

--- pages/api/list.ts
+++ pages/api/list.ts
@@ -1,11 +1,10 @@
 import { connectToDatabase } from "../../utils/mongodb";
-import type { NextApiRequest, NextApiResponse } from 'next'
-
+import type { NextApiRequest, NextApiResponse } from "next";
+import { parse } from "json2csv";
 export default async (req: NextApiRequest, res: NextApiResponse<JSON>) => {
     const { db } = await connectToDatabase();
-    const registrationForms = await db
-        .collection("registration")
-        .find({})
-        .toArray();
-    res.json(registrationForms);
+    const registrationForms = await db.collection("registration").find({}).toArray();
+    const rslt: string = parse(registrationForms).toString();
+    // @ts-ignore
+    res.send(rslt);
 };
