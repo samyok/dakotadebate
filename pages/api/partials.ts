c04bfabd1ab83920dfa7d7a193703b414a84e1e5
Samyok Nepal <nepalsamyok@gmail.com>
2021-06-26 23:29:42 -0500

    add csv to partials

--- pages/api/partials.ts
+++ pages/api/partials.ts
@@ -1,11 +1,11 @@
 import { connectToDatabase } from "../../utils/mongodb";
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { parse } from "json2csv";
 
 export default async (req: NextApiRequest, res: NextApiResponse<JSON>) => {
     const { db } = await connectToDatabase();
-    const forms = await db
-        .collection("partials")
-        .find({})
-        .toArray();
-    res.json(forms);
+    const forms = await db.collection("partials").find({}).toArray();
+    const rslt: string = parse(forms).toString();
+    // @ts-ignore
+    res.send(rslt);
 };
