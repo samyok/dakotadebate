cc97c82356021f445af1747a310f8b9157e715d0
Samyok Nepal <samyok@fiveable.me>
2022-03-20 13:32:43 -0400

    ignore eslint during builds

--- next.config.js
+++ next.config.js
@@ -1,20 +1,23 @@
 module.exports = {
-    reactStrictMode: true,
-    images: {
-        minimumCacheTTL: 60 * 60 * 24 * 30, // cache for a month
-    },
-    async redirects() {
-        return [
-            {
-                source: '/apply',
-                destination: 'https://forms.gle/pr8dBF4z6F2RtNc29',
-                permanent: false,
-            },
-            {
-                source: '/scholarships',
-                destination: 'https://forms.gle/RK7WppSmTnAR6qxz8',
-                permanent: false,
-            }
-        ]
-    }
+  reactStrictMode: true,
+  images: {
+    minimumCacheTTL: 60 * 60 * 24 * 30, // cache for a month
+  },
+  async redirects() {
+    return [
+      {
+        source: "/apply",
+        destination: "https://forms.gle/pr8dBF4z6F2RtNc29",
+        permanent: false,
+      },
+      {
+        source: "/scholarships",
+        destination: "https://forms.gle/RK7WppSmTnAR6qxz8",
+        permanent: false,
+      },
+    ];
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
